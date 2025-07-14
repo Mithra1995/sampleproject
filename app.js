33810d71986ac1a6504f36a719658b63f90b67aa
Mithra1995 <mithra.balasubramaniam05@gmail.com>
2025-07-14 02:35:29 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
+const express = require('express');
+const app = express();
+const PORT = 80;
+
+app.get('/', (req, res) => {
+  res.send('<h1>Hello from ECS Fargate!</h1>');
+});
+
+app.listen(PORT, () => {
+  console.log(`App is running on port ${PORT}`);
+});
 
